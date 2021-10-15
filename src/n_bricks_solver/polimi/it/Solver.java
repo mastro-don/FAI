b80ec01e9a31a7b3a80f1cc271cd81f77f77e493
mastro-don <78470370+mastro-don@users.noreply.github.com>
2021-10-15 16:29:50 +0200

    Ho paura di benz

---
+++ src/n_bricks_solver/polimi/it/Solver.java
@@ -0,0 +1,16 @@
+package n_bricks_solver.polimi.it;
+
+public class Solver {
+    private Game InternalGame;
+
+    void public Solver(Game InternalGame){
+        this.InternalGame = InternalGame;
+    }
+
+    public SolverMinimax(){
+        int[] Actions;
+        Actions[] = InternalGame.getState().getActions;
+
+    }
+
+}
