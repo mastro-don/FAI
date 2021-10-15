1b1516a8b19ce1649401cee87f3385f56749e035
Ax-Time <giacomo1307@gmail.com>
2021-10-15 16:49:11 +0200

    Fourth Commit

--- src/n_bricks_solver/polimi/it/StateSevenBricks.java
+++ src/n_bricks_solver/polimi/it/StateSevenBricks.java
@@ -4,7 +4,9 @@ import ai_concepts.polimi.it.Action;
 import ai_concepts.polimi.it.State;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class StateSevenBricks extends State{
     private List<Pile> piles;
@@ -14,14 +16,28 @@ public class StateSevenBricks extends State{
         this.piles = new ArrayList<>();
     }
 
+    public StateSevenBricks(List<Pile> piles){
+        this.piles = piles;
+    }
+
     @Override
     public List<Action> getActions() {
         List<Action> possibleActions = new ArrayList<>();
 
-        for(Pile p : piles){
-            if(p.getHeight() > minHeight){
-
+        for(int i = piles.size() - 1; i >= 0; i--){
+            if(piles.get(i).getHeight() > minHeight){
+                // The pile can be splitted
+                for(int j = 1; j < (float) piles.get(i).getHeight(); j++){
+                    List<Pile> currPiles = new ArrayList<>(piles);
+                    int pileHeight = piles.get(i).getHeight();
+                    currPiles.remove(i);
+                    currPiles.add(new Pile(j));
+                    currPiles.add(new Pile(pileHeight - j));
+                    possibleActions.add(new Action(this, new StateSevenBricks(currPiles)));
+                }
             }
         }
+
+        return possibleActions;
     }
 }
