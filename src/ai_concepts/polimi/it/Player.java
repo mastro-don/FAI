c966c3c55e7dc9ea97221d84b459802a7dbf783d
Ax-Time <giacomo1307@gmail.com>
2021-10-15 19:13:40 +0200

    implemented equals method for Player class

--- src/ai_concepts/polimi/it/Player.java
+++ src/ai_concepts/polimi/it/Player.java
@@ -1,5 +1,11 @@
 package ai_concepts.polimi.it;
 
+import org.jetbrains.annotations.NotNull;
+
 public enum Player{
-    PLAYER1, PLAYER2
+    PLAYER1, PLAYER2;
+
+    public boolean equals(@NotNull Player p){
+        return this.PLAYER1.ordinal() == p.ordinal();
+    }
 }
