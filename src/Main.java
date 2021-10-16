efcd1e61aaa52b02ddec94d0d8cb02eb19d19b04
mastro-don <78470370+mastro-don@users.noreply.github.com>
2021-10-16 03:08:09 +0200

    Input da linea di comando pt_2

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,12 @@ import n_bricks_solver.polimi.it.StateSevenBricks;
 
 public class Main {
     public static void main(String[] args){
-        GameSevenBricks game = new GameSevenBricks(new StateSevenBricks(Integer.parseInt(args[0])));
-        SolverSevenBricks s = new SolverSevenBricks(game);
-        Action a = s.solverMinimax(game.getCurrentState());
-
+        do{
+            GameSevenBricks game = new GameSevenBricks(new StateSevenBricks(7);
+            SolverSevenBricks s = new SolverSevenBricks(game);
+            Action a = s.solverMinimax(game.getCurrentState());
+            System.in.rea
+        }while(game.getCurrentState().isGoal());
         // Print best action
         ((StateSevenBricks) a.getStart()).print();
         ((StateSevenBricks) a.getDest()).print();
