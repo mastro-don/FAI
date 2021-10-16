047f13977b7f85300dfa392a74b95ca068e2ce90
mastro-don <78470370+mastro-don@users.noreply.github.com>
2021-10-16 03:03:56 +0200

    Input da linea di comando

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
 import ai_concepts.polimi.it.Action;
-import ai_concepts.polimi.it.State;
 import n_bricks_solver.polimi.it.GameSevenBricks;
-import n_bricks_solver.polimi.it.Pile;
 import n_bricks_solver.polimi.it.SolverSevenBricks;
 import n_bricks_solver.polimi.it.StateSevenBricks;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args){
-        GameSevenBricks game = new GameSevenBricks(new StateSevenBricks(7));
+        GameSevenBricks game = new GameSevenBricks(new StateSevenBricks(Integer.parseInt(args[0])));
         SolverSevenBricks s = new SolverSevenBricks(game);
         Action a = s.solverMinimax(game.getCurrentState());
 
