38c15a832b68be5ae5b5bceb6d02f344a3669838
mastro-don <78470370+mastro-don@users.noreply.github.com>
2021-10-16 03:14:12 +0200

    Changestate

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,24 @@ import n_bricks_solver.polimi.it.StateSevenBricks;
 
 public class Main {
     public static void main(String[] args){
+        int Dividi;
         do{
             GameSevenBricks game = new GameSevenBricks(new StateSevenBricks(7);
             SolverSevenBricks s = new SolverSevenBricks(game);
             Action a = s.solverMinimax(game.getCurrentState());
-            System.in.rea
+            // Print best action
+            ((StateSevenBricks) a.getStart()).print();
+            ((StateSevenBricks) a.getDest()).print();
+            game.changeCurrentState(a.getDest());
+
+            // My action
+
+            Dividi = System.in.read();
+
+
+
         }while(game.getCurrentState().isGoal());
-        // Print best action
-        ((StateSevenBricks) a.getStart()).print();
-        ((StateSevenBricks) a.getDest()).print();
+
+
     }
 }
--- src/ai_concepts/polimi/it/Game.java
+++ src/ai_concepts/polimi/it/Game.java
@@ -5,11 +5,15 @@ import org.jetbrains.annotations.NotNull;
 public class Game {
     private State currentState;
 
-   public Game(@NotNull State currentState){
+    public Game(@NotNull State currentState){
         this.currentState = currentState;
    }
 
     public State getCurrentState() {
         return this.currentState;
+    }
+
+    public void changeCurrentState(State nextState){
+        this.currentState = nextState;
     }
 }
